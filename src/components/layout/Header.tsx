fb8ce2b69775df6ecd01e7a6fe190482c6b31369
Tejaswee Sahu <tejasweesahu5504@gmail.com>
2025-06-13 18:18:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo iconSize=24

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -21,7 +21,7 @@ export const Header: React.FC = () => {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center"> {/* Main flex container */}
+      <div className="container mx-auto px-6 flex h-16 items-center"> {/* Main flex container with horizontal padding */}
         {/* Left section for Logo */}
         <div className="flex items-center">
           <Logo iconSize={24} textSize="text-xl" />
